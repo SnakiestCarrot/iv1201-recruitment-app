a12c13d8186a152141c8edf82d174c351ff9eb3c
Casper Johansson <147274068+SnakiestCarrot@users.noreply.github.com>
2026-01-21 13:21:13 +0100

    Frontend service running
    Look at localhost:3000 to see the react+vite default page

---
+++ frontend-service/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Needed for Hot Reload in Docker on some systems
+    },
+    host: true,       // Expose to Docker network (0.0.0.0)
+    strictPort: true,
+    port: 3000,       // We want port 3000, not 5173
+  }
+})
