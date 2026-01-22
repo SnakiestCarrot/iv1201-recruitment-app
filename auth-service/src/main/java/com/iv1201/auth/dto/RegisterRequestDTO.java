ab14d7f3c03c19f016120d8efe6e6c35e3f01c43
AugustWikdahl <159790745+AugustWikdahl@users.noreply.github.com>
2026-01-22 14:43:43 +0100

    Created RegisterRequestDTO for capturing input data from client

---
+++ auth-service/src/main/java/com/iv1201/auth/dto/RegisterRequestDTO.java
@@ -0,0 +1,41 @@
+package com.iv1201.auth.dto;
+
+public class RegisterRequestDTO {
+
+    private String username;
+    private String password;
+
+
+    /**
+     * Sets the username.
+     * @param username The desired username.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Sets the password.
+     * @param password The raw password.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    /**
+     * Gets the username.
+     * @return The username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Gets the password.
+     * @return The password.
+     */
+    public String getPassword() {
+        return password;
+    }
+}
