3892563180ab3f4af217260c8dd7a8b651caa57f
Casper Johansson <147274068+SnakiestCarrot@users.noreply.github.com>
2026-01-28 15:42:14 +0100

    added vitest for frontend unit testing

--- frontend-service/vite.config.ts
+++ frontend-service/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', // We will create this next
+  },
   server: {
     watch: {
       usePolling: true, // Needed for Hot Reload in Docker on some systems
