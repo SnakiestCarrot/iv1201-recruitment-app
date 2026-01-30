fb8335cd17d188fe83b2759a046d22c309fc4f0d
Casper Johansson <147274068+SnakiestCarrot@users.noreply.github.com>
2026-01-30 11:42:21 +0100

    Fix testing bug

--- recruitment-service/src/test/java/com/iv1201/recruitment/repository/CompetenceProfileRepositoryTest.java
+++ recruitment-service/src/test/java/com/iv1201/recruitment/repository/CompetenceProfileRepositoryTest.java
@@ -6,6 +6,7 @@ import com.iv1201.recruitment.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class CompetenceProfileRepositoryTest {
 
     @Autowired
